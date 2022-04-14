a97f69ac7de6af43a2fe258b6e234ed35d1791fe
apriorifox <apriorifox@gmail.com>
2022-04-14 15:07:07 +0600

    made main banner

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
-import React from "react"
-import MainBanner from "../mainBanner"
-import Cifras from "../cifras"
-import Footer from "../footer"
+import React from "react";
+import MainBanner from "../mainBanner";
+import Cifras from "../cifras";
+import Footer from "../footer";
 import '../../styles/core.css'
 
 
--- src/components/mainBanner/mainBanner.js
+++ src/components/mainBanner/mainBanner.js
@@ -1,21 +1,35 @@
 import React from "react";
+import LeftSide from '../../images/mainBannerImages/leftSide.svg';
+import RightSide from '../../images/mainBannerImages/rightSide.svg';
+import BackSide from '../../images/mainBannerImages/backSide.svg'
 import './mainBanner.css'
 
 const MainBanner = () => {
  return (
      <div className="mainBanner">
-            <span className="yellowElipse"></span>
-            {/* <span className="leftSide"></span> */}
-            {/* <span className="rightSide"></span> */}
-            {/* <span className="backSide"></span> */}
+        <div className="caps">
+            <div className="leftElems">
+                <span className="yellowElipse"></span>
+                <img src={LeftSide} alt='leftSide' className="leftSide"/>
+            </div>
+            <div className="rightElems">
+                <img src={RightSide} alt="rightSide" className="rightSide"/>
+                <div className="right">
+                    <img src={BackSide} alt="backSide" className="backSide"/>
+                    <span className="redElipse">
+                        <span className="blick"></span>
+                    </span>
+                </div>
+            </div> 
+        </div>
         <div className="container">
-            <din className = "mainBannerText">
+            <div className = "mainBannerText">
                 <h2>NEW ERA</h2>
                 <p>Новая коллекция 2021 уже доступны на заказ
     яркие цвета, винтажный принт 70х, тематические 
     группы и отличное качество</p>
                 <a href = "#" className="button">Открыть каталог</a>
-            </din>
+            </div>
         </div>
      </div>
  )
